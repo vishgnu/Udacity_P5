b371d244a25a625956a8f4a3aeede234211031b8
vishgnu <niels.walter@gmail.com>
2015-12-08 00:27:12 +0100

    simple ko binding working

--- MyDash/Scripts/main.js
+++ MyDash/Scripts/main.js
@@ -1,4 +1,36 @@
-﻿
+﻿// Class to represent a row in the seat reservations grid
+function Restaurant(name, kitchenType) {
+    var self = this;
+    self.name = name;
+    self.typeOfKitchen = ko.observable(kitchenType);
+}
+
+
+// Overall viewmodel for this screen, along with initial state
+function RestaurantFinderViewModel() {
+    var self = this;
+
+    // Non-editable catalog data - would come from the server
+    self.kitchenTypes = [
+        { type: "Italian Restaurant"},
+        { type: "Thai Streetkitchen"},
+        { type: "Burger Joint" },
+        { type: "Greekt Grill" }
+    ];
+
+    // Searchable data
+    self.Restaurants = ko.observableArray([
+        new Restaurant("The Stallion", self.kitchenTypes[0]),
+        new Restaurant("Thai Ngam", self.kitchenTypes[1]),
+        new Restaurant("Walt's Diner", self.kitchenTypes[2]),
+        new Restaurant("Apollon Grill", self.kitchenTypes[3])
+    ]);
+}
+
+// run knockout
+ko.applyBindings(new RestaurantFinderViewModel());
+
+
 var map = {
     "display": function displayMap() {
 
