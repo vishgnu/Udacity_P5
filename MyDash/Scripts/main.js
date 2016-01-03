bf1d1e37c76503d19daa8b1ea12428b45f5935f8
vishgnu <niels.walter@gmail.com>
2016-01-03 22:37:05 +0100

    reataurant filter working

--- MyDash/Scripts/main.js
+++ MyDash/Scripts/main.js
@@ -1,5 +1,77 @@
-﻿(function () {
+﻿var map;    // declares a global map variable
+
+
+// helper
+var stringContains = function (string, contains) {
+    string = string || "";
+    contains = contains || "";
+    if (contains.length > string.length)
+        return -1;
+    return string.indexOf(contains);
+};
+
+(function () {
     'use strict';
+
+     /*
+    Start here! initializeMap() is called when page is loaded.
+    */
+    function initializeMap() {
+
+        var locations;
+
+        var mapOptions = {
+            disableDefaultUI: true,
+            center: {lat: 51.4502796, lng: 6.6406621},
+            zoom: 13
+        };
+
+        var mapdiv = document.getElementById("map-div");
+        map = new google.maps.Map(mapdiv, mapOptions);
+
+        // Sets the boundaries of the map based on pin locations
+        window.mapBounds = new google.maps.LatLngBounds();
+
+        addMarker();
+    }
+
+
+
+    function addMarker() {
+        // The next lines save location data from the search result object to local variables
+        var location = { "lat": 51.4494309, "lng": 6.6436319 };
+
+        // marker is an object with additional data about the pin for a single location
+        var marker = new google.maps.Marker({
+            map: map,
+            position: location,
+            title: "test"
+        });
+
+        //infoWindows are the little helper windows that open when you click
+        //or hover over a pin on a map. They usually contain more information
+        //about a location.
+        var infoWindow = new google.maps.InfoWindow({
+            content: "<div> HELLO WORLD </div>"
+        });
+
+        // add event handling for mapmarkers
+        google.maps.event.addListener(marker, 'click', function () {
+            infoWindow.open(map, marker);
+        });
+    }
+
+
+    // Calls the initializeMap() function when the page loads
+    window.addEventListener('load', initializeMap);
+
+    // Vanilla JS way to listen for resizing of the window
+    // and adjust map bounds
+    window.addEventListener('resize', function (e) {
+        //Make sure the map bounds get updated on page resize
+        map.fitBounds(mapBounds);
+    });
+
 
     // Class to represent a single restaurant
     var Restaurant = function (name, kitchenType) {
@@ -14,14 +86,40 @@
         // keep track of self
         var self = this;
 
-        this.restaurants = ko.observableArray(restaurants);
-        //this.restaurants = ko.observableArrary(restaurants.map(function (restaurant) {
-        //    var testres = restaurant;
+        // our data
+        this.allRestaurants = ko.observableArray(restaurants);
+       
+        // store data from input for filtering list of restaurants
+        self.currentSearchFilter = ko.observable();
 
-        //    return new Restaurant(restaurant.name, restaurant.typeOfKitchen)
-        //}));
+        self.restaurants = ko.computed(function () {
+            if (!self.currentSearchFilter()) {
+                return self.allRestaurants();
+            } else {
+                    
+                // filter names here
+                //return self.allRestaurants()[0];
 
+                return ko.utils.arrayFilter(self.allRestaurants(), function (rest) {
 
+                    var indexName = stringContains(rest.name.toLowerCase(), self.currentSearchFilter().toLowerCase());
+                    var indexType = stringContains(rest.typeOfKitchen().type.toLowerCase(), self.currentSearchFilter().toLowerCase());
+
+                    if (indexName != -1 || indexType != -1) {
+                        return true;
+                    }
+                    else {
+                        return false;
+                    };
+                });
+            }
+        });
+
+        // clear existing filter and show all data again
+        self.clearSearch = function () {
+            self.currentSearchFilter('');
+            return;
+        };
     }
 
     // some masterdata
@@ -50,14 +148,10 @@
         new Restaurant("Bua Luang", kitchenTypes[1])
     ];
 
-
-
 // bind a new instance of our view model to the page
 var viewModel = new ViewModel(restaurants || []);
 
 ko.applyBindings(viewModel);
-
-    showMarkers();
 
 }());
 
