0aefd71842d507ed45a0270267246eafcda6334f
vishgnu <niels.walter@gmail.com>
2016-01-31 22:08:09 +0100

    ko binding gets the reference of map go on

--- MyDash/Scripts/main.js
+++ MyDash/Scripts/main.js
@@ -1,104 +1,16 @@
-﻿
-// helper
-var stringContains = function (string, contains) {
-    string = string || "";
-    contains = contains || "";
-    if (contains.length > string.length)
-        return -1;
-    return string.indexOf(contains);
-};
+﻿    'use strict';
 
+    //global for map
+    var map;
 
-
-//self.InitializeAllMarkers = function () {
-//    // The next lines save location data from the search result object to local variables
-//    var location = { "lat": 51.4494309, "lng": 6.6436319 };
-
-//    // marker is an object with additional data about the pin for a single location
-//    var marker = new google.maps.Marker({
-//        map: map,
-//        position: location,
-//        title: "test"
-//    });
-
-//    //infoWindows are the little helper windows that open when you click
-//    //or hover over a pin on a map. They usually contain more information
-//    //about a location.
-//    var infoWindow = new google.maps.InfoWindow({
-//        content: "<div> HELLO WORLD </div>"
-//    });
-
-//    // add event handling for mapmarkers
-//    google.maps.event.addListener(marker, 'click', function () {
-//        infoWindow.open(map, marker);
-//    });
-
-//}
-
-//ko.bindingHandlers.marker = {
-
-//    infowindow: null,
-
-//    init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-
-//        var mapDiv = document.getElementById("map-div");
-//        var map = "";//bindingContext.$root.getMap();
-//        var restaurant = valueAccessor().restaurant;
-//        var latLng = new google.maps.LatLng(restaurant.lat, restaurant.long);
-//        var marker = new google.maps.Marker({
-//            position: latLng,
-//            map: map,
-//            icon: "http://chart.apis.google.com/chart?chst=d_bubble_icon_text_small_withshadow&chld=swim|bbT|" + restaurant.name + "|24527A|E0EBEB",
-//            title: restaurant.name
-//        });
-
-//        marker.addListener('click', function () {
-//            ko.bindingHandlers.marker.openInfoWindow(map, marker);
-//        });
-//    },
-
-
-
-//    openInfoWindow: function (map, marker) {
-//        var contentString = '<div">' + marker.getTitle() + '</div>';
-//        if (this.infowindow) {
-//            this.infowindow.close();
-//        };
-//        this.infowindow = new google.maps.InfoWindow({
-//            content: contentString,
-//            pixelOffset: new google.maps.Size(50, 0),
-//        });
-//        map.setZoom(9);
-//        map.setCenter(marker.getPosition());
-//        this.infowindow.open(map, marker);
-//        google.maps.event.addListener(this.infowindow, 'closeclick', function () {
-//            map.setZoom(7);
-//            //map.setCenter(mapOptions.center);
-//        });
-//    }
-//};
-
-
-
-/* Represents a Google Map object */
-var GoogleMap = function (center, element) {
-    var self = this;
-
-        var mapOptions = {
-            center: center,
-            zoom: 13
-        };
-
-    // assign a google maps element
-    map = new google.maps.Map(element, mapOptions);
-
-   
-    return map;
-};
-
-
-(function () {
-    'use strict';
+    // helper
+    var stringContains = function (string, contains) {
+        string = string || "";
+        contains = contains || "";
+        if (contains.length > string.length)
+            return -1;
+        return string.indexOf(contains);
+    };
 
     // Class to represent a single restaurant
     var Restaurant = function (name, kitchenType) {
@@ -116,18 +28,6 @@ var GoogleMap = function (center, element) {
 
         // keep track of self
         var self = this;
-
-        // initialize defaults
-        var map,
-            mapCanvas = $('#map-div')[0],
-            // center moers
-            center = new google.maps.LatLng(51.4502796, 6.6406621);
-
-
-        function initialize() {
-            map = GoogleMap(center, mapCanvas);
-            //fetchMeetups(meetupApiUrl);
-        }
 
         // our data
         this.allRestaurants = ko.observableArray(restaurants);
@@ -159,9 +59,42 @@ var GoogleMap = function (center, element) {
             self.currentSearchFilter('');
             return;
         };
+    }
 
-        // initialization listener
-        google.maps.event.addDomListener(window, 'load', initialize);
+
+
+    ko.bindingHandlers.map = {
+        init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+
+            var gm = allBindingsAccessor().map;
+
+            //var position = new google.maps.LatLng(allBindingsAccessor().latitude(), allBindingsAccessor().longitude());
+
+            //var marker = new google.maps.Marker({
+            //    map: allBindingsAccessor().map,
+            //    position: position,
+            //    title: name
+            //});
+
+            //viewModel._mapMarker = marker;
+        },
+        //update: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+        //    var latlng = new google.maps.LatLng(allBindingsAccessor().latitude(), allBindingsAccessor().longitude());
+        //    viewModel._mapMarker.setPosition(latlng);
+
+        //}
+    };
+
+
+
+    function createMap() {
+        var elevator;
+        var myOptions = {
+            zoom: 3,
+            center: new google.maps.LatLng(12.24, 24.54),
+            mapTypeId: 'terrain'
+        };
+        map = new google.maps.Map($('#map-div')[0], myOptions);
     }
 
     // some masterdata
@@ -193,10 +126,11 @@ var GoogleMap = function (center, element) {
 // bind a new instance of our view model to the page
 var viewModel = new ViewModel(restaurants || []);
 
-// run ko bindings
-ko.applyBindings(viewModel);
+$(document).ready(function () {
 
+    createMap();
+    ko.applyBindings(viewModel);
+});
 
-}());
 
 
