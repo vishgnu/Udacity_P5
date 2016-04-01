a28788f3be0ed3fdc6f1e7f0a2eebb290790fd82
vishgnu <niels.walter@gmail.com>
2016-04-02 00:27:12 +0200

    clenaup3

--- MyDash/Scripts/main.js
+++ MyDash/Scripts/main.js
@@ -2,15 +2,20 @@
 
 //create global for map
 var map;
-
 // current instance of infowindow
 var infowindow;
 
+
 // main viewmodel for this screen, along with initial state
-var ViewModel = function(vacations) {
+var ViewModel = function (vacations) {
 
     // keep track of self
     var self = this;
+    
+    // keep maker
+    var activeMarker;
+
+
 
     // our complete "database"
     this.allVacations = ko.observableArray(vacations);
@@ -56,7 +61,7 @@ var ViewModel = function(vacations) {
 
         // update mapmarkers here for all found search results
         for (var i = 0; i < filteredVacations.length; i++) {
-            console.log(filteredVacations[i].name + " - create marker !");
+            //console.log(filteredVacations[i].name + " - create marker !");
 
             // create marker for all 
             var marker = new google.maps.Marker({
@@ -70,12 +75,7 @@ var ViewModel = function(vacations) {
             google.maps.event.addListener(marker, 'click', (function (marker, map, i) {
                 return function () {
 
-                    // run animation for click
-                    if (marker.getAnimation() !== null) {
-                        marker.setAnimation(null);
-                    } else {
-                        marker.setAnimation(google.maps.Animation.BOUNCE);
-                    }
+                    activeMarker = marker;
 
                     // zoommin on map
                     map.setZoom(viewModel.vacations()[i].zoom);
@@ -88,15 +88,13 @@ var ViewModel = function(vacations) {
 
                     // add eventhandler for closing
                     google.maps.event.addListener(infowindow, 'closeclick', function () {
-                        map.setZoom(3);
-                        map.setCenter(new google.maps.LatLng(startCoordinates.lat, startCoordinates.long));
-                        marker.setIcon();
-                        marker.setAnimation(google.maps.Animation.DROP);
+                        self.resetMap(marker);
+
                     });
 
                     // get some data from worldweather for the clicked location via datamodel url and the makertitle
                     $.ajax({
-                    url: weatherApiURL.replace("!%%MarkerTitle%%!",marker.title),
+                        url: weatherApiURL.replace("!%%MarkerTitle%%!", marker.title),
                         type: 'POST',
                         data: { location: i, name: marker.title },
                         contentType: 'application/json; charset=utf-8',
@@ -131,11 +129,26 @@ var ViewModel = function(vacations) {
         //return search result for binding to list
         return filteredVacations;
     });
-    
+
     // clear existing filter and show all data again by resetting the search filter
     self.clearSearch = function () {
+        // reset input
         self.currentSearchFilter('');
+        self.resetMap();
         return;
+    };
+
+    self.resetMap = function (marker) {
+        infowindow.close();
+        map.setZoom(3);
+        map.setCenter(new google.maps.LatLng(startCoordinates.lat, startCoordinates.long));
+        if (marker != null) {
+            marker.setIcon();
+            activeMarker = null;
+        }
+        else if (activeMarker != null) {
+            activeMarker.setIcon();
+        }
     };
 
     // handle click on list of locations
@@ -146,16 +159,12 @@ var ViewModel = function(vacations) {
         if (this.lastClick != vacation.name) {
 
             this.lastClick = vacation.name;
-
             new google.maps.event.trigger(vacation.mapMarker, 'click');
         }
         else {
             this.lastClick = "";
-            infowindow.close();
-            map.setZoom(3);
-            map.setCenter(new google.maps.LatLng(startCoordinates.lat, startCoordinates.long));
-            vacation.mapMarker.setIcon();
-            marker.setAnimation(google.maps.Animation.DROP);
+            self.resetMap(vacation.mapMarker);
+
         }
     };
 }
@@ -173,7 +182,7 @@ function createMap() {
     map = new google.maps.Map($('#map-div')[0], myOptions);
 
     // set gloabl infowindow
-    infowindow =  new google.maps.InfoWindow({
+    infowindow = new google.maps.InfoWindow({
         content: loadingContent,
         pixelOffset: new google.maps.Size(10, 0),
     });
